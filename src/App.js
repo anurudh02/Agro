25f06fdd39ff7e262902317fd31e0565512eac9d
anurudh02 <anurudhdeepan@gmail.com>
2024-02-08 20:40:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Layout } from 'antd';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import AppHeader from './components/common/header';
+import AppHome from './pages/home';
+import AppContact from './pages/contact';
+import AppAbout from './pages/about';
+import AppShop from './pages/shop';
+import AppFaq from './pages/faq';
+import FooterWidget from './components/common/footerWidget';
+import FooterCopyright from './components/common/footerCopyright';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout>
+        <Router>
+          <Header>
+            <AppHeader />
+          </Header>
+          <Content>
+            <Routes>
+            <Route path='/' element={<AppHome/>}/>
+              <Route path='/Agro' element={<AppHome/>}/>
+              <Route path='/about' element={<AppAbout/>}/>
+              <Route path='/shop' element={<AppShop/>}/>
+              <Route path='/faq' element={<AppFaq/>}/>
+              <Route path='/contact' element={<AppContact/>}/>
+            </Routes>
+          </Content>
+        </Router>
+        
+        <Footer>
+          <FooterWidget/>
+          <FooterCopyright/>
+        </Footer>
+      </Layout>
     </div>
   );
 }
